f64db3a8e4fb1d3e2d4bd17b28eb33abffd4bdca
Manuel <manuelheller@outlook.com>
2020-08-08 19:42:40 +0200

    testig best future reward

--- test.py
+++ test.py
@@ -2,6 +2,73 @@ import math
 import random
 import time
 
+class Nim():
+
+    def __init__(self, initial=[1, 3, 5, 7]):
+        """
+        Initialize game board.
+        Each game board has
+            - `piles`: a list of how many elements remain in each pile
+            - `player`: 0 or 1 to indicate which player's turn
+            - `winner`: None, 0, or 1 to indicate who the winner is
+        """
+        self.piles = initial.copy()
+        self.player = 0
+        self.winner = None
+
+    @classmethod
+    def available_actions(cls, piles):
+        """
+        Nim.available_actions(piles) takes a `piles` list as input
+        and returns all of the available actions `(i, j)` in that state.
+
+        Action `(i, j)` represents the action of removing `j` items
+        from pile `i` (where piles are 0-indexed).
+        """
+        actions = set()
+        for i, pile in enumerate(piles):
+            for j in range(1, pile + 1):
+                actions.add((i, j))
+        return actions
+
+    @classmethod
+    def other_player(cls, player):
+        """
+        Nim.other_player(player) returns the player that is not
+        `player`. Assumes `player` is either 0 or 1.
+        """
+        return 0 if player == 1 else 1
+
+    def switch_player(self):
+        """
+        Switch the current player to the other player.
+        """
+        self.player = Nim.other_player(self.player)
+
+    def move(self, action):
+        """
+        Make the move `action` for the current player.
+        `action` must be a tuple `(i, j)`.
+        """
+        pile, count = action
+
+        # Check for errors
+        if self.winner is not None:
+            raise Exception("Game already won")
+        elif pile < 0 or pile >= len(self.piles):
+            raise Exception("Invalid pile")
+        elif count < 1 or count > self.piles[pile]:
+            raise Exception("Invalid number of objects")
+
+        # Update pile
+        self.piles[pile] -= count
+        self.switch_player()
+
+        # Check for a winner
+        if all(pile == 0 for pile in self.piles):
+            self.winner = self.player
+
+
 class NimAI():
 
     def __init__(self, alpha=0.5, epsilon=0.1):
@@ -44,21 +111,37 @@ class NimAI():
         """
         new_q = reward + future_rewards
         self.q[tuple(state), action] = old_q + self.alpha * (new_q - old_q)
+    
+    def best_future_reward(self, state):
+        """
+        Given a state `state`, consider all possible `(state, action)`
+        pairs available in that state and return the maximum of all
+        of their Q-values.
 
+        Use 0 as the Q-value if a `(state, action)` pair has no
+        Q-value in `self.q`. If there are no available actions in
+        `state`, return 0.
+        """
+        actions = Nim.available_actions(state)
+        values = []
+        for action in actions:
+            values.append(self.get_q_value(state,action))
+        if len(values) == 0:
+            return 0
+        else:
+            return max(values)
 
 ai = NimAI()
 
-ai.q[(1, 1, 4, 4), (0, 2)] = 0.75
+ai.q[(1, 1, 4, 4), (3, 2)] = 0.75
+ai.q[(1, 1, 4, 4), (3, 4)] = 0.5
+ai.q[(1, 1, 4, 4), (0, 1)] = 0
+ai.q[(1, 1, 4, 6), (3, 2)] = 1
 
-state = [1, 1, 4, 4]
-action = (0,2)
+state = [1, 1, 4, 5]
+#state = [1, 1, 4, 4]
+#state = [0, 0, 0, 0]
 
-old_q = ai.get_q_value(state,action)
-reward = 0
-future_rewards = 1
-
-ai.update_q_value(state, action, old_q, reward, future_rewards)
-
-print(ai.q)
+print(ai.best_future_reward(state))
 
 
