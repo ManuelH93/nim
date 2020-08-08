64aeda136b9b4fb93aa4f6eb22202e42e4e7d7f9
Manuel <manuelheller@outlook.com>
2020-08-08 17:05:06 +0200

    understand given code

---
+++ test.py
@@ -0,0 +1,48 @@
+class Nim():
+
+    def __init__(self, initial=[1, 3, 5, 7]):
+        """
+        Initialize game board.
+        Each game board has
+            - `piles`: a list of how many elements remain in each pile
+            - `player`: 0 or 1 to indicate which player's turn
+            - `winner`: None, 0, or 1 to indicate who the winner is
+        """
+        self.piles = initial.copy()
+        self.player = 0
+        self.winner = None
+
+    @classmethod
+    def available_actions(cls, piles):
+        """
+        Nim.available_actions(piles) takes a `piles` list as input
+        and returns all of the available actions `(i, j)` in that state.
+
+        Action `(i, j)` represents the action of removing `j` items
+        from pile `i` (where piles are 0-indexed).
+        """
+        actions = set()
+        for i, pile in enumerate(piles):
+            for j in range(1, pile + 1):
+                actions.add((i, j))
+        return actions
+
+    @classmethod
+    def other_player(cls, player):
+        """
+        Nim.other_player(player) returns the player that is not
+        `player`. Assumes `player` is either 0 or 1.
+        """
+        return 0 if player == 1 else 1
+    
+    def switch_player(self):
+        """
+        Switch the current player to the other player.
+        """
+        self.player = Nim.other_player(self.player)
+
+
+game = Nim()
+print(game.player)
+game.switch_player()
+print(game.player)
